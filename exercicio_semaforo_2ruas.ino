5b6e92d58fd36b8e84102bc86c5c268cadcf3b50
DiegoAlvesprogramer <99756612+DiegoAlvesprogramer@users.noreply.github.com>
2022-03-12 17:39:49 -0300

    exercício semáforo 2 ruas

--- exercicio_semaforo_2ruas.ino
+++ exercicio_semaforo_2ruas.ino
@@ -1,48 +1,50 @@
 // C++ code
 //
 /*
-  This program blinks pin 12 of the Arduino (the
-  built-in LED)
+  Projeto de semaforo de 2 ruas
 */
-#define LedVM	10,8 // define um nome por outro
-#define LedAM	12,7 // define um nome por outro
-#define LedVD	13,6  // define um nome por outro
+#define LedVM	10 // define um nome por outro
+#define LedAM	12 // define um nome por outro
+#define LedVD	13 // define um nome por outro
+#define LEDVM	8 // define um nome por outro
+#define LEDAM	6 // define um nome por outro
+#define LEDVD	3 // define um nome por outro
 #define ON		HIGH
 #define OFF		LOW
 int tempo=1000; // variável (memória) = 1000
 
 void setup()
 {
-  pinMode(10, OUTPUT); // coloca o pino do arduino como saída
-  pinMode(12, OUTPUT); // coloca o pino do arduino como saída
-  pinMode(13, OUTPUT); // coloca o pino do arduino como saída
-  pinMode(8, OUTPUT); // coloca o pino do arduino como saída
-  pinMode(6, OUTPUT); // coloca o pino do arduino como saída
-  pinMode(3, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LedVM, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LedAM, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LedVD, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LEDVM, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LEDAM, OUTPUT); // coloca o pino do arduino como saída
+  pinMode(LEDVD, OUTPUT); // coloca o pino do arduino como saída
 }
 
 void loop()
 {
-
-  digitalWrite(10, ON); // acende o led vermelho
-  digitalWrite(3, ON); // acende o led verde
-  delay(tempo); // Wait for 1000 millisecond(s)
-  digitalWrite(3, OFF); // apaga led verde
-  digitalWrite(10, OFF); // apago led vermelho
-  delay(tempo); // Wait for 1000 millisecond(s)
   
-  digitalWrite(6, ON); // acende o led amarelo
-  digitalWrite(12, ON); // acende o led amarelo
-  delay(tempo); // Wait for 1000 millisecond(s)
-  digitalWrite(6, OFF); // apaga led amarelo
-  digitalWrite(12, OFF); // apago led vermelho
-  delay(tempo); // Wait for 1000 millisecond(s)
- 
+  digitalWrite(LedVM, ON); // acende o led vermelho
+  digitalWrite(LEDVD, ON); // acende o led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
+  digitalWrite(LedVM, OFF); // apago led vermelho
+  digitalWrite(LEDVD, OFF); // apago led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
   
-  digitalWrite(13, ON); // acende o led vermelho
-  digitalWrite(8, ON); // acende o led verde
-  delay(tempo); // Wait for 1000 millisecond(s)
-  digitalWrite(13, OFF); // apaga led vermelho
-  digitalWrite(8, OFF); // apaga led verde
-  delay(tempo); // Wait for 1000 millisecond(s)
+  digitalWrite(LedAM, ON); // acende o led vermelho
+  digitalWrite(LEDAM, ON); // acende o led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
+  digitalWrite(LedAM, OFF); // apago led vermelho
+  digitalWrite(LEDAM, OFF); // apago led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
+  
+  digitalWrite(LEDVM, ON); // acende o led vermelho
+  digitalWrite(LedVD, ON); // acende o led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
+  digitalWrite(LEDVM, OFF); // apago led vermelho
+  digitalWrite(LedVD, OFF); // apago led vermelho
+  delay(tempo); // Tempo de 1000 milisegundos
+  
 }
